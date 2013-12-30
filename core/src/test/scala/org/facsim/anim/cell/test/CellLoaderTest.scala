0124f7a6ad87c1a6d50d44e1230b1d1715e2a3a4
Mike Allen <mike.allen@facsim.org>
2013-12-29 19:10:15 -0500

    Corrected CellLoaderTest.
    Updated CellLoaderTest, which had become out-of-date. Also updated and
    revised some of the cell test file resources.

--- core/src/test/scala/org/facsim/anim/cell/test/CellLoaderTest.scala
+++ core/src/test/scala/org/facsim/anim/cell/test/CellLoaderTest.scala
@@ -42,7 +42,7 @@ import java.io.FileNotFoundException
 import java.net.URL
 import org.facsim.anim.cell.CellLoader
 import org.scalatest.FunSpec
-import org.scalatest.PrivateMethodTester
+import scala.annotation.tailrec
 
 //=============================================================================
 /**
@@ -50,153 +50,75 @@ Test suite for the [[org.facsim.anim.cell.CellLoader]] class.
 */
 //=============================================================================
 
-class CellLoaderTest extends FunSpec with PrivateMethodTester {
+class CellLoaderTest extends FunSpec {
 
 /**
-Trait for default cell loader object.
+Function to retrieve a test resource file's URL.
+
+Files should be present in src/test/resources/cellFiles.
 */
 
-  trait DefaultCellLoader {
-    val defaultLoader = new CellLoader ()
+  def testRscFile (file: String) = getClass.getResource("cellFiles/" + file)
+
+/**
+Trait populated with cell file URL's to be loaded.
+*/
+
+  trait CellFiles {
+    val files = List (
+      testRscFile ("ArcCoarse.cell"),
+      testRscFile ("ArcCoarseReverse.cell"),
+      testRscFile ("ArcFine.cell"),
+      testRscFile ("ArcFineReverse.cell"),
+      testRscFile ("CircleCoarseSolid.cell"),
+      testRscFile ("CircleCoarseWire.cell"),
+      testRscFile ("CircleFineSolid.cell"),
+      testRscFile ("CircleFineWire.cell"),
+      testRscFile ("ConeCoarse.cell"),
+      testRscFile ("ConeFine.cell"),
+      testRscFile ("CylinderCoarse.cell"),
+      testRscFile ("CylinderFine.cell"),
+      testRscFile ("FrustumCoarse.cell"),
+      testRscFile ("FrustumFine.cell"),
+      testRscFile ("HemisphereCoarse.cell"),
+      testRscFile ("HemisphereFine.cell"),
+      testRscFile ("ScreenFastText.cell"),
+      testRscFile ("ScreenNormalText.cell"),
+      testRscFile ("SectorCoarse.cell"),
+      testRscFile ("SectorCoarseReverse.cell"),
+      testRscFile ("SectorFine.cell"),
+      testRscFile ("SectorFineReverse.cell"),
+      testRscFile ("Tetrahedron.cell"),
+      testRscFile ("Trapezoid.cell"),
+      testRscFile ("Triad.cell"),
+      testRscFile ("UnrotateFastText.cell"),
+      testRscFile ("UnrotateNormalText.cell"),
+      testRscFile ("WorldText.cell")
+    )
   }
 
 /*
 Test fixture description.
 */
 
-  describe (classOf [CellLoader].getCanonicalName) {
+  describe (CellLoader.getClass.getCanonicalName) {
 
 /*
-Default constructor tests.
+Cell file load tests.
 */
 
-    describe (".this ()") {
-      it ("must construct without error") {
-        new DefaultCellLoader {
-        }
-      }
-      it ("must report default load flags") {
-        new DefaultCellLoader {
-          assert (defaultLoader.getFlags () === 0)
-        }
-      }
-      it ("must report null base path") {
-        new DefaultCellLoader {
-          assert (defaultLoader.getBasePath () === null)
-        }
-      }
-      it ("must report null base url") {
-        new DefaultCellLoader {
-          assert (defaultLoader.getBaseUrl () === null)
-        }
-      }
-    }
-
-/*
-load (String) tests.
-*/
-
-    describe (".load (String)") {
-      it ("must throw NullPointerException if passed a null file name") {
-        new DefaultCellLoader {
-          val nullFile: String = null
-          intercept [NullPointerException] {
-            defaultLoader.load (nullFile)
+    describe (".load (URL, SafeOption[URL], CellColor.Value, CellColor.Value)")
+    {
+      it ("must load all files with default arguments") {
+        new CellFiles {
+          @tailrec
+          def loadFile (list: List[URL]): Unit = {
+            if (list.isEmpty) {
+              CellLoader.load (list.head)
+              loadFile (list.tail)
+            }
           }
-        }
-      }
-      it ("must throw FileNotFoundException if file does not exist") {
-        new DefaultCellLoader {
-          intercept [FileNotFoundException] {
-            defaultLoader.load ("IBMissing.cell")
-          }
-        }
-      }
-    }
-
-/*
-load (URL) tests.
-*/
-
-    describe (".load (URL)") {
-      it ("must throw NullPointerException if passed a null URL") {
-        new DefaultCellLoader {
-          val nullUrl: URL = null
-          intercept [NullPointerException] {
-            defaultLoader.load (nullUrl)
-          }
-        }
-      }
-      it ("must throw FileNotFoundException if file does not exist") {
-        new DefaultCellLoader {
-          intercept [FileNotFoundException] {
-            defaultLoader.load (new URL ("file:///IBMissing.cell"))
-          }
-        }
-      }
-    }
-
-/*
-setFlags (Int) tests.
-
-This function is deprecated.  We have to call it via reflection (using
-ScalaTest's private method invoker) to avoid compiler warnings about calling a
-deprecated function.
-*/
-
-    describe (".setFlags (Int) - DEPRECATED") {
-      it ("must throw UnsupportedOperationException when called") {
-        val setFlags = PrivateMethod [Unit] ('setFlags)
-        new DefaultCellLoader {
-          val thrown = intercept [UnsupportedOperationException] {
-            defaultLoader invokePrivate setFlags (0)
-          }
-          assert (thrown.getMessage === "TODO I18N: CellLoader.setFlags () " +
-          "is deprecated.")
-        }
-      }
-    }
-
-/*
-setBasePath (String) tests.
-
-This function is deprecated.  We have to call it via reflection (using
-ScalaTest's private method invoker) to avoid compiler warnings about calling a
-deprecated function.
-*/
-
-    describe (".setBasePath (String) - DEPRECATED") {
-      it ("must throw UnsupportedOperationException when called") {
-        val setBasePath = PrivateMethod [Unit] ('setBasePath)
-        val default: String = "."
-        new DefaultCellLoader {
-          val thrown = intercept [UnsupportedOperationException] {
-            defaultLoader invokePrivate setBasePath (default)
-          }
-          assert (thrown.getMessage === "TODO I18N: CellLoader.setBasePath () "
-          + "is deprecated.")
-        }
-      }
-    }
-
-/*
-setBaseUrl (String) tests.
-
-This function is deprecated.  We have to call it via reflection (using
-ScalaTest's private method invoker) to avoid compiler warnings about calling a
-deprecated function.
-*/
-
-    describe (".setBaseUrl (String) - DEPRECATED") {
-      it ("must throw UnsupportedOperationException when called") {
-        val setBaseUrl = PrivateMethod [Unit] ('setBaseUrl)
-        val default: URL = new URL ("http://facsim.org/")
-        new DefaultCellLoader {
-          val thrown = intercept [UnsupportedOperationException] {
-            defaultLoader invokePrivate setBaseUrl (default)
-          }
-          assert (thrown.getMessage === "TODO I18N: CellLoader.setBaseUrl () "
-          + "is deprecated.")
+          loadFile (files)
         }
       }
     }
